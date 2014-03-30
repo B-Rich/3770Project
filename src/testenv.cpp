1d18fec499ecdc8dddb904ff15b30901d5c8aba2
Will-Beninger <ArchTyriel@hotmail.com>
2014-03-29 19:47:03 -0600

    Fully functioning test area that signals when things are clicked

--- src/testenv.cpp
+++ src/testenv.cpp
@@ -1,28 +1,23 @@
 #include "testenv.h"
 
-TestEnv::TestEnv(QWidget *parent) : QWidget(parent)
+TestEnv::TestEnv(int num, int dist, QWidget *parent) : QWidget(parent), totalTargets(num), moveDistance(dist)
 {
    started = false;
    timer = new QTime();
+   curTarget = new QRect(0,0,20,20);
+   qsrand((uint)QTime::currentTime().msec());
+   setMinimumSize(500, 500);
 }
 
 void TestEnv::start()
 {
    started = true;
+   totalTime=0;
+   remainingTargets = totalTargets;
+   newTarget();
+   
    timer->start();
    update();
-}
-
-void TestEnv::stop()
-{
-   started = false;
-   totalTime = timer->elapsed();
-   update();
-}
-
-void TestEnv::reset()
-{
-   totalTime = 0;
 }
 
 int TestEnv::retTime()
@@ -34,4 +29,78 @@ void TestEnv::paintEvent(QPaintEvent *event)
 {
    QPainter painter(this);
    painter.setRenderHint(QPainter::Antialiasing, true);
+   painter.setBrush(palette().foreground().color());
+   if (started)
+      painter.drawRect(*curTarget);
+   QWidget::paintEvent(event);
+}
+
+void TestEnv::mousePressEvent(QMouseEvent *event)
+{
+   if (!started)
+   {
+      QWidget(event);
+   }
+   else
+   {
+      if (curTarget->contains(event->pos()))
+      {
+         emit TestEnv::emitHit(--remainingTargets);
+         if (remainingTargets == 0)
+         {
+            TestEnv::stop();
+         }
+         else
+         {
+            newTarget();
+            update();
+         }
+      }
+   }
+}
+
+void TestEnv::stop()
+{
+   started = false;
+   totalTime = timer->elapsed();
+
+   update();
+}
+
+void TestEnv::newTarget()
+{
+   QPoint curPos = TestEnv::mapFromGlobal(QCursor::pos());
+   bool notDone = true;
+   while (notDone)
+   {
+      int degreesToMove = qrand() % 360;
+      double tempDegree = degreesToMove%90;
+      
+      double radiansToMove = (tempDegree * PI) /180.0;
+      int moveX = (int)(qCos(radiansToMove) * moveDistance);
+      int moveY = (int)(qSin(radiansToMove) * moveDistance);
+
+      QPoint newPos;
+      if (degreesToMove >= 0 && degreesToMove <=90)
+      {
+         newPos = QPoint((curPos.x() + moveX), (curPos.y() - moveY));
+      }
+      else if (degreesToMove >90 && degreesToMove <= 180)
+      {
+         newPos = QPoint((curPos.x() - moveX), (curPos.y() - moveY));
+      }
+      else if (degreesToMove >180 && degreesToMove <= 270)
+      {
+         newPos = QPoint((curPos.x() - moveX), (curPos.y() + moveY));
+      }
+      else
+      {
+         newPos = QPoint((curPos.x() + moveX), (curPos.y() + moveY));
+      }
+      curTarget->moveTo(newPos);
+      if (QWidget::frameGeometry().contains(*curTarget, true))
+      {
+         notDone = false;
+      }
+   }
 }
